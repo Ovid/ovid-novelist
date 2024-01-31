d9d7248220786422f4300b36a91f64c541a453fe
Ovid <curtis.poe@gmail.com>
2024-01-31 09:15:51 +0100

    remove unused variables

--- tests/model/test_novel.py
+++ tests/model/test_novel.py
@@ -25,8 +25,8 @@ class TestNovel(unittest.TestCase):
     def test_delete_chapter(self):
         chapter1 = Chapter("Chapter 1", "This is the first chapter.")
         chapter2 = Chapter("Chapter 2", "This is the second chapter.")
-        new_chapter1 = self.novel.add_chapter(chapter1)
-        new_chapter2 = self.novel.add_chapter(chapter2)
+        self.novel.add_chapter(chapter1)
+        self.novel.add_chapter(chapter2)
         self.novel.delete_chapter(chapter1)
         self.assertEqual(len(self.novel.get_chapters()), 1)
         self.assertEqual(self.novel.get_chapters()[0].title, "Chapter 2")
